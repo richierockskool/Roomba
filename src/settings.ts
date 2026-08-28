9d7acad46f598a68e95c1e260a38bfc5a4c51dd8
Rich <38402006+richierockskool@users.noreply.github.com>
2026-08-28 16:01:19 -0400

    Update platform and plugin names for Homebridge

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,10 @@
 /**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ * Platform name used in Homebridge config.json.
  */
-export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
+export const PLATFORM_NAME = 'Roomba';
 
 /**
- * This must match the name of your plugin as defined the package.json `name` property
+ * Plugin package name.
+ * Must exactly match the "name" property in package.json.
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = 'homebridge-roomba-pro';
